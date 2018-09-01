0e069acc6a9da80a18345e3da7ef23596a277e9a
Curtis Burkhalter <curtisburkhalter@gmail.com>
2018-09-01 15:57:09 -0400

    Remove variables and create correlation plot
    This commit removes variables from the WNS dataset that have more than 25% of their data missing, as well as variables that exhibit no variation.
    
    Additional code added creates a correlation plot for the numeric covariates.

--- Susceptibility_Modeling/Scripts/Data_cleanup.R
+++ Susceptibility_Modeling/Scripts/Data_cleanup.R
@@ -2,62 +2,64 @@
 suppressMessages(library(tidyverse))
 suppressMessages(library(janitor))
 suppressMessages(library(here))
+suppressMessages(library(scales))
+suppressMessages(library(ggcorrplot))
 
 #read in data file containing the White Nose Syndrome status of each North American species;
 #data file also includes associated habitat and behavioral data
-
-wns_raw <- read_csv(here("WNS_Projects","Susceptibility_Modeling","Data","NABats_WNS.csv")) %>%
-  clean_names()
-
-#there are a bunch of extra rows and columns so remove those
-wns_raw <- wns_raw[1:48,1:19]
-
-#need to change some column names
-colnames(wns_raw)[1] <- "species"
-colnames(wns_raw)[grep(".*min_temp",colnames(wns_raw))] <- "min_temp"
-colnames(wns_raw)[grep(".*develop",colnames(wns_raw))] <- "disease_present"
-colnames(wns_raw)[grep(".*percent",colnames(wns_raw))] <- "change_lambda"
-
-#need to change any row entries that are either 'unk' or 'NULL' to an NA
-wns_raw[wns_raw == "unk"|wns_raw == "Null"] <- NA
-
-#change 'disease_present' to factor
-wns_raw$disease_present <- as.factor(wns_raw$disease_present)
-
-#write function that checks if a column is binary or not; because the binary columns were
-#read in as character you have to use the numbers in quotes as opposed to 'x in 0:1' as if 
-#they were numeric
-bin_check <- function(x) {
-  all(x == "0"|x=="1" | is.na(x)) 
-}
-
-#apply the 'bin_check' function to the columns of wns_raw and subset the column names
-#where TRUE
-bin_vars <- colnames(wns_raw)[apply(wns_raw,2,bin_check)]
-
-#using the subsetted column names in 'bin_var' convert all the selected columns to factors
-wns_raw[,bin_vars] <- lapply(wns_raw[,bin_vars], as.factor)
-
-#I need to convert min_temp, max_temp and change_lambda to numeric;
-#first build function that checks to see if a column is of type character
-#and if any of the column entries contain digits
- num_check <- function(x) {
-   is.character(x) & any(grepl("\\d",x))
- }
- 
-#using the 'num_check' function subset the column names that satisfy the conditions
-#laid out in the funciton; when applying the num_check function we have to use
-#sapply versus apply. This is because when using is.character(x) the dataframe is
-#coerced to a matrix and the factor variables are then treated as character which 
-#in turn returns any column that is a factor, which in this case is most of the covariates
-num_vars <- colnames(wns_raw)[sapply(wns_raw, num_check)]
- 
-wns_raw[,num_vars] <- lapply(wns_raw[,num_vars], as.numeric)
-
-wns_cleaned <- wns_raw
-
-write_csv(wns_cleaned,here("WNS_Projects","Susceptibility_Modeling","Data","Cleaned_WNS_file.csv"),col_names = TRUE)
-
+# 
+# wns_raw <- read_csv(here("WNS_Projects","Susceptibility_Modeling","Data","NABats_WNS.csv")) %>%
+#   clean_names()
+# 
+# #there are a bunch of extra rows and columns so remove those
+# wns_raw <- wns_raw[1:48,1:19]
+# 
+# #need to change some column names
+# colnames(wns_raw)[1] <- "species"
+# colnames(wns_raw)[grep(".*min_temp",colnames(wns_raw))] <- "min_temp"
+# colnames(wns_raw)[grep(".*develop",colnames(wns_raw))] <- "disease_present"
+# colnames(wns_raw)[grep(".*percent",colnames(wns_raw))] <- "change_lambda"
+# 
+# #need to change any row entries that are either 'unk' or 'NULL' to an NA
+# wns_raw[wns_raw == "unk"|wns_raw == "Null"] <- NA
+# 
+# #change 'disease_present' to factor
+# wns_raw$disease_present <- as.factor(wns_raw$disease_present)
+# 
+# #write function that checks if a column is binary or not; because the binary columns were
+# #read in as character you have to use the numbers in quotes as opposed to 'x in 0:1' as if 
+# #they were numeric
+# bin_check <- function(x) {
+#   all(x == "0"|x=="1" | is.na(x)) 
+# }
+# 
+# #apply the 'bin_check' function to the columns of wns_raw and subset the column names
+# #where TRUE
+# bin_vars <- colnames(wns_raw)[apply(wns_raw,2,bin_check)]
+# 
+# #using the subsetted column names in 'bin_var' convert all the selected columns to factors
+# wns_raw[,bin_vars] <- lapply(wns_raw[,bin_vars], as.factor)
+# 
+# #I need to convert min_temp, max_temp and change_lambda to numeric;
+# #first build function that checks to see if a column is of type character
+# #and if any of the column entries contain digits
+#  num_check <- function(x) {
+#    is.character(x) & any(grepl("\\d",x))
+#  }
+#  
+# #using the 'num_check' function subset the column names that satisfy the conditions
+# #laid out in the funciton; when applying the num_check function we have to use
+# #sapply versus apply. This is because when using is.character(x) the dataframe is
+# #coerced to a matrix and the factor variables are then treated as character which 
+# #in turn returns any column that is a factor, which in this case is most of the covariates
+# num_vars <- colnames(wns_raw)[sapply(wns_raw, num_check)]
+#  
+# wns_raw[,num_vars] <- lapply(wns_raw[,num_vars], as.numeric)
+# 
+# wns_cleaned <- wns_raw
+# 
+# write_csv(wns_cleaned,here("WNS_Projects","Susceptibility_Modeling","Data","Cleaned_WNS_file.csv"),col_names = TRUE)
+# 
 
 ################################################################
 ################################################################
@@ -107,7 +109,7 @@ NAs_bycol <- as.data.frame(t(sub_ww %>%
 
 #attach a new column name 'count' which is the count of NAs for the 
 #variable across all 34 records of sub_ww
-colnames(NAs_bycol)[1] <- "count"
+colnames(NAs_bycol)[1] <- "count of covariate missing"
 
 #attach a new column name 'covariate' which is the name of the
 #covariates using the row names already attached
@@ -120,20 +122,65 @@ row.names(NAs_bycol) <- NULL
 #each covariate
 NAs_bycol$pctNA <- round((NAs_bycol$count/nrow(sub_ww))*100,digits = 1)
 
+#write to file
+#write_csv(NAs_bycol , path = here("WNS_Projects","Susceptibility_Modeling","Data","Covariate_DataMissingAcrossSpecies.csv"), col_names = TRUE)
+
 #determine how many rows of NAs_bycol have 0% covariate data missing
 nrow(NAs_bycol[NAs_bycol$pctNA < 0.0001,])
 summary(NAs_bycol)
 
+
+#count NAs by row for the data where status is not an NA;
+#this tells us how much data is missing for each record
+NAs_byrow <- as.data.frame(apply(sub_ww, 1, function(x) sum(is.na(x))))
+colnames(NAs_byrow) <- "count of covariate missing"
+
+#determine the percentage of covariate data that is an NA for
+#each record using ncol - 2, one of the columns is simply
+#an index and doesn't really count as a covariate and one is the
+#species name; add species name to each row as well
+NAs_byrow$pctNA <- round((NAs_byrow$count/(ncol(sub_ww)-2))*100,digits = 1)
+NAs_byrow <- cbind(NAs_byrow,sub_ww[,2])
+colnames(NAs_byrow)[3] <- "species"
+
+#look at summary of NAs_byrow$pctNA
+summary(NAs_byrow$pctNA)
+
+#write_csv(NAs_byrow , path = here("WNS_Projects","Susceptibility_Modeling","Data","Covariate_DataMissingBySpecies.csv"), col_names = TRUE)
+
+
 #begin the process of cleaning up the covariate data
 
+#get vector of column names where NAs make up >= 25% of records
+missing_25ormore <- NAs_bycol$covariate[NAs_bycol$pctNA >= 25]
+
+column_selectors <- paste(".*", missing_25ormore, sep = "")
+column_selectors <- paste(column_selectors, collapse = "|")
+
+ww_cov25_removed <- sub_ww[, -grep(column_selectors, colnames(sub_ww))]
+
 #after looking the diet and activity variables, all of the species
-#are 100% nectar feeding and 100%nocturnal so we can't use any of the
+#are 100% nectar feeding and 100 % nocturnal so we can't use any of the
 #diet or activity variables
-sub_ww <- sub_ww[,-grep(".*diet_i|.*diet_n|.*diet_v|.*diet_s|.*diet_f|.*diet_p|.*activity_crep|.*activity_noc|.*activity_diur",colnames(sub_ww))]
+removed_ww <- ww_cov25_removed[,-grep(".*diet_i|.*diet_n|.*diet_v|.*diet_s|.*diet_f|.*diet_p|.*activity_crep|.*activity_noc|.*activity_diur",colnames(ww_cov25_removed))]
 
 #all of the species that we have records for in terms of their WNS status
 #are in the family Vespertilionidae so we can remove all of the family
-#names because those are useless now due to lack of variation
-sub_ww <- sub_ww[,-grep(".*idae",colnames(sub_ww))]
+#names because those are useless now due to lack of variation; also all remaining species are in the order
+#Chiroptera so we can remove the order column; also remove the species portion the name because that doesn't really
+#provide any information
+removed_ww <- removed_ww[,-grep(".*idae|order|species",colnames(removed_ww))]
 
-nrow(NAs_bycol[NAs_bycol$pctNA >= 50,])
+#need to estimate correlation between numeric variables
+
+#first get just the numeric variables
+nums <- unlist(lapply(removed_ww, is.numeric))  
+
+#subset the 'removed_ww' dataframe to only include the numerics using 'nums' as index
+num_ww <- removed_ww[, nums]
+
+#calculate correlation removing the first column because that is just an index column
+corr <- round(cor(num_ww[,-1], use = "pairwise.complete.obs"), 1)
+
+#plot correlations using ggcorrplot
+ggcorrplot(corr)
