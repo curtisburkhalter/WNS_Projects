7ac7fd196b7a70eec0e5c80531d1c92f0df0763c
Curtis Burkhalter <curtisburkhalter@gmail.com>
2018-09-10 06:28:23 -0400

    Change response variable name
    This commit changes the response variable throughout the script

--- Susceptibility_Modeling/Scripts/Boosted_regression_tree_analysis.R
+++ Susceptibility_Modeling/Scripts/Boosted_regression_tree_analysis.R
@@ -9,41 +9,37 @@ suppressMessages(library(e1071))
 suppressMessages(library(RANN))
 suppressMessages(library(caTools))
 
-#read in data file containing the White Nose Syndrome status of each North American species;
-#data file has been previously cleaned
-wns_clean <- read_csv(here("WNS_Projects","Susceptibility_Modeling","Data","Cleaned_WNS_file.csv")) %>%
+#read in data file containing the White Nose Syndrome status of each species;
+#data file has been previously cleaned and missing data imputed
+wns_clean <- read_csv(here("WNS_Projects","Susceptibility_Modeling","Data","Imputed_Dataset.csv")) %>%
   clean_names()
 
-#the only values that wns_clean$quarries provides is either a 0 or an NA, as such
-#it provides no information so remove it
-wns_clean$quarries <- NULL
+#remove the 'method' column
+wns_clean$method <- NULL
 
-#subset the columns where the data is only of type integer, in this case it is the binary columns
-int_vars <- colnames(wns_clean)[sapply(wns_clean,is.integer)]
+#package gbm requires that the response variable, in this case 'wns_status'
+#to be in an integer but make sure not to standardize it
 
-#package gbm requires taht the response variable, in this case 'disease present'
-#to be in an integer so remove it from int_vars
-int_vars <- int_vars[-grep(".*disease",int_vars)]
+#convert columns 'genus' and 'for_strat_value' to factors
+wns_clean$genus <- as.factor(wns_clean$genus)
+wns_clean$for_strat_value <- as.factor(wns_clean$for_strat_value)
 
-#for the columns that were subsetted into the vector 'int_vars' convert to a factor using
-#lapply function
-wns_clean[,int_vars] <- lapply(wns_clean[,int_vars],as.factor)
-
-#need to remove change in lambda from the dataset b/c it leads to 
-#class separation
-wns_clean <- wns_clean[,-grep(".*change",colnames(wns_clean))]
+#class separation in classification is when the coding of a variable or something as simply
+#as the labels themselves perfectly allow for prediction of different classes and has nothing
+#correlative or causal to do with the response variable; need to be vigilant for this in
+#the variables
 
 #need to scale the numeric variables so that are comparable in terms of mean and 
 #deviation to the binary variables; to do this use the 'arm' package; first
 #subset those columns that are numeric and remove the response variable from result
 num_cols <- colnames(wns_clean)[sapply(wns_clean, is.numeric)]
-num_cols <- num_cols[-grep(".*dise",num_cols)]
+num_cols <- num_cols[-grep(".*wns_st",num_cols)]
 
 wns_clean[,num_cols] <- apply(wns_clean[,num_cols],2,rescale)
 
 #need to create a stratified random sample for the k-fold cross validation
 set.seed(10)
-TrainSplits <- unlist(createDataPartition(wns_clean$disease_present, p = 0.80))
+TrainSplits <- unlist(createDataPartition(wns_clean$wns_status, p = 0.80))
 
 train_set <- wns_clean[TrainSplits,]
 test_set <- wns_clean[-TrainSplits,]
@@ -51,7 +47,7 @@ test_set <- wns_clean[-TrainSplits,]
 #subset train_set to not include species identity as a covariate
 sub_train <- train_set[,-1]
 
-sub_train$disease_present <- as.factor(sub_train$disease_present)
+sub_train$wns_status <- as.factor(sub_train$wns_status)
 sub_train <-as.data.frame(sub_train)
 
 #build a parameter tuning grid; the 3 parameters that should be tuned for a 
@@ -73,13 +69,13 @@ fitControl <- trainControl(
 #specify the training settings for the parameter tuning; would be interesting to see 
 #how this changes with imputation of missing values
 set.seed(825)
-gbmTune <- train(disease_present ~ ., data = sub_train,  
+gbmTune <- train(wns_status ~ ., data = sub_train,  
                  method = "gbm", 
                  trControl = fitControl, 
-                 preProcess = c("bagImpute"),
+                 #preProcess = c("bagImpute"),
                  verbose = FALSE, 
                  tuneGrid = gbmGrid,
-                 na.action = na.pass,
+                 #na.action = na.pass,
                  metric = "kappa")
 
 #look at tuning results
